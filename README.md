21343df4ae1c2ef4b07f078c4a9d7d0b47db3f02
Similar Su <similarsu@qq.com>
2018-11-07 17:20:14 +0800

    init jpa

--- README.md
+++ README.md
@@ -5,4 +5,5 @@
 happy-core:核心模块
 happy-concurrency:并发编程学习
 happy-lombok: lombok使用
+happy-jpa: jpa
 ```
---
+++ happy-jpa/src/main/java/net/similarsu/happyjpa/HappyJpaApplication.java
@@ -0,0 +1,12 @@
+package net.similarsu.happyjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyJpaApplication.class, args);
+	}
+}
